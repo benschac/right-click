76da1ac358322013f224c79d1e42983365ffc161
Benjamin Schachter <benschac@gmail.com>
2021-12-04 20:31:59 -0500

    fiddle around with open props

--- client/src/Root.tsx
+++ client/src/Root.tsx
@@ -1,10 +1,21 @@
 import React from "react";
-import { Link } from "react-router-dom";
+import css from "./Root.module.css";
 
 export const Root: React.FC = () => {
   return (
-    <div>
-      Root Component <Link to="/about">About</Link>
-    </div>
+    <>
+      <header>
+        <nav className={css.header}>
+          <span className={css.logo}>right-click</span>
+          {/* Play about with grid a little more */}
+          <div className={css.app}>app</div>
+          <div className={css.about}>about</div>
+          <div className={css.github}>github</div>
+        </nav>
+      </header>
+      <main>
+        <h1>Right Click</h1>
+      </main>
+    </>
   );
 };
